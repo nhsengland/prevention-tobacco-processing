cccc1cf23c74b92f5aa4c8976a5bc41c82d241a5
Jack Hodgson <110386394+JackHodgson95@users.noreply.github.com>
2024-07-22 13:45:53 +0100

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,100 +1,30 @@
-# Databricks notebook source
-# MAGIC %run ./config
-
-# COMMAND ----------
-
-# MAGIC %run ./functions
-
-# COMMAND ----------
-
-### collect data
-# patient level
-#prepare_source_parquets(root_path = root_path_patient,
- #                       schema_path = schema_path_patient,
-  #                      file_prefix = file_prefix_patient,
-   #                     out_path = raw_path_patient,
-    #                    min_date = min_date,
-     #                   max_date = max_date)
-
-# patient level
-read_pld(out_path = raw_path_patient,
-        min_date = min_date,
-        max_date = max_date)
-
-# aggregate level
-prepare_source_parquets(root_path = root_path_aggregate,
-                        schema_path = schema_path_aggregate,
-                        file_prefix = file_prefix_aggregate,
-                        out_path = raw_path_aggregate,
-                        min_date = min_date,
-                        max_date = max_date)
-
-# patient level tobacco data
-clean_patient(raw_path = raw_path_patient,
-              out_path = clean_path_patient,
-              min_date = min_date,
-              max_date = max_date)
-
-# sus data
-read_sus(min_date = min_date,
-         max_date = max_date,
-         out_path = sus_path)
-
-# msds data 
-read_msds(min_date = min_date,
-          max_date = max_date, 
-          out_path = msds_path)
-
-# COMMAND ----------
-
+%run ./config
+%run ./functions
+# collect patient level data
+read_pld()
+# collect aggregate level data
+read_agg()
+# clean patient level tobacco data
+clean_patient()
 ### build provider reference data
-provider_ref(clean_path = clean_path_patient,
-             out_path = ref_prov_path)
-
-# COMMAND ----------
-
+provider_ref()
 ### build commissioner reference data
-commissioner_ref(clean_path = clean_path_patient,
-                 out_path = ref_com_path)
-
-# COMMAND ----------
-
+commissioner_ref()
+# collect msds data 
+read_msds()
+# collect mhsds data 
+read_mhsds()
+# collect sus apc data
+read_sus()
 ### build elements
-build_elements(clean_path = clean_path_patient,
-               out_path = element_path)
-
-# COMMAND ----------
-
+build_elements()
 ### build metrics
-build_metrics(element_path = element_path,
-              out_path = metric_path)
-
-# COMMAND ----------
-
+build_metrics()
 ### aggregate data
-aggregate_metrics(metric_path = metric_path, 
-                  out_path = metric_aggregate_path)
-
-# COMMAND ----------
-
+aggregate_metrics()
 ### build tableau metric dataset
-build_tableau_metrics(metric_path = metric_aggregate_path,
-                      out_path = tableau_metric_path)
-
-# COMMAND ----------
-
+build_tableau_metrics()
 ### build tableau dq dataset
-build_tableau_dq(out_path = tableau_dq_path)
-
-# COMMAND ----------
-
+build_tableau_dq()
 ### build tableau pathways dataset
-build_tableau_pathways(out_path = tableau_pathways_path,
-                       out_path_raw = tableau_pathways_raw_path)
-
-# COMMAND ----------
-
-### build sof measures
-build_sof(min_date = min_date,
-          max_date = max_date,
-          out_path = sof_path)
+build_tableau_pathways()
